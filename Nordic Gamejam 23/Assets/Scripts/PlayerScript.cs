12bb6ac23ff93c48dbd995a61e0838547e13f287
Otterfan02 <91531814+Otterfan02@users.noreply.github.com>
2023-04-15 00:00:22 +0200

    Added respawn and score

--- Nordic Gamejam 23/Assets/Scripts/PlayerScript.cs
+++ Nordic Gamejam 23/Assets/Scripts/PlayerScript.cs
@@ -1,19 +1,32 @@
 using Coherence.Toolkit;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
+    [Header("Movement")]
     private Rigidbody2D rb;
     public float speed;
     public float dragConstant;
+    
+    [Header("Heat")]
     private float maxHeat = 69420;
     private float currentHeat;
     public float decayRate;
     public GameObject heatBar;
+    
+    [Header("Connection")]
     private bool isConnected;
     private CoherenceMonoBridge bridge;
+
+    [Header("Score")]
+    public TMP_Text scoreText;
+    private float score;
+
+    [Header("Div")]
+    private Vector2 StartPos;
 
 
 
@@ -23,6 +36,7 @@ public class PlayerScript : MonoBehaviour
         rb = gameObject.GetComponent<Rigidbody2D>();
         currentHeat = maxHeat;
         isConnected = false;
+        StartPos = transform.position;
     }
 
     // Update is called once per frame
@@ -52,17 +66,30 @@ public class PlayerScript : MonoBehaviour
         {
             currentHeat -= decayRate * Time.deltaTime;
         }
+
+        if(currentHeat <= 0)
+        {
+            score = 0;
+            transform.position = StartPos + new Vector2(Random.RandomRange(-20, 20), Random.RandomRange(-20, 20));
+            currentHeat = maxHeat;
+        }
+
         Debug.Log(currentHeat);
 
         if (!CheckConnected())
         {
             currentHeat = maxHeat;
+            score = 0;
         }
         
 
         heatBar.gameObject.transform.localScale = new Vector3(currentHeat/maxHeat, heatBar.gameObject.transform.localScale.y, heatBar.gameObject.transform.localScale.z);
 
         rb.velocity = rb.velocity * dragConstant;
+
+        scoreText.text = "Score: " + Mathf.Round(score);
+
+        score += Time.deltaTime;
 
 
     bool CheckConnected()
