ea7b22b21e885582c3dc06ae045dfb5b59ab0499
Otterfan02 <91531814+Otterfan02@users.noreply.github.com>
2023-04-15 06:14:30 +0200

    Eg kogte

--- Nordic Gamejam 23/Assets/Scripts/NeedleScript.cs
+++ Nordic Gamejam 23/Assets/Scripts/NeedleScript.cs
@@ -7,14 +7,14 @@ public class NeedleScript : MonoBehaviour
     // Start is called before the first frame update
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        Debug.Log(collision.transform.tag);
-        if(collision.transform.tag == "Enemy")
+        Debug.Log(collision.collider.transform.tag);
+        if (collision.transform.tag == "Enemy")
         {
             //Debug.Log("Kill");
             transform.parent.GetComponent<PlayerScript>().score += 69;
             transform.parent.GetComponent<PlayerScript>().currentHeat += 6900;
             Destroy(collision.gameObject);
-        } else if(collision.transform.tag == "Player")
+        } else if (collision.collider.gameObject.CompareTag("Player"))
         {
             transform.parent.GetComponent<PlayerScript>().score += collision.gameObject.GetComponent<PlayerScript>().score;
             transform.parent.GetComponent<PlayerScript>().currentHeat += collision.gameObject.GetComponent<PlayerScript>().currentHeat;
